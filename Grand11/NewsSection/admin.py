c55dfafe1a52aaf7d3b64a46beed6524db43ba2d
SelnoxInfotech <117904460+SelnoxInfotech@users.noreply.github.com>
2022-12-29 11:35:45 +0530

    Add files via upload

---
+++ Grand11/NewsSection/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Category,SubCategory,News,VideoNews
+
+
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(News)
+admin.site.register(VideoNews)
+
+
+
+# Register your models here.
